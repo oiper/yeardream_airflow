1c6baf9804e09c933f7377c7f4b12bf45092b04e
“oiper <oipingfox@gmail.com>
2024-06-18 09:59:33 +0900

    none

--- dags/dags_bash_operator_standard.py
+++ dags/dags_bash_operator_standard.py
@@ -1,7 +1,24 @@
 import datetime
 from airflow.decorators import dag
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+import pendulum
 
-@dag(start_date=datetime.datetime(2023, 1, 1, tz="Asia/Seoul"), schedule="0 0 * * *", catchup=False)
+@dag(dag_id='dags_bash_operator_decorator',
+    start_date=pendulum.datetime(2023, 1, 1, tz="Asia/Seoul"),
+    schedule="0 0 * * *",
+    catchup=False,
+    tags=['homework']
+)
 def generate_dag():
-    EmptyOperator(tesk_id="first")
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami"
+    )
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_cmmand="echo $HOSTNAME"
+    )
+
+
+    bash_t1 >> bash_t2
+
